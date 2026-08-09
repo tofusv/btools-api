c946c2cbdc69048d4255b62b4e6c3d3798924adf
tofusv <122224158+tofusv@users.noreply.github.com>
2026-08-09 19:07:02 +0700

    Update app.py

--- btools_webapp/app.py
+++ btools_webapp/app.py
@@ -37,9 +37,11 @@ def get_template_docx_path():
     return None
 
 def call_gemini_api(raw_text: str, api_key: str) -> dict:
-    models_to_try = [
-        "gemini-1.5-pro",
-        "gemini-1.5-flash-8b"
+        models_to_try = [
+        "gemini-1.5-flash-latest",
+        "gemini-1.5-flash-001",
+        "gemini-1.5-flash",
+        "gemini-pro"
     ]
     
     prompt = """
